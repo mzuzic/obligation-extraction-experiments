39e501a36069a6cb8eb419ce55196be9951f999a
markozuzic <mzuzic@apttus.com>
2020-06-29 21:32:07 +0200

    Modify setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 mkdir data_test
-curl -L -o data_test/data.csv "https://drive.google.com/file/d/1sq43KgDLrZZ3mdTmoey4XGfuiOIt8FCi/view?usp=sharing"
+wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1sq43KgDLrZZ3mdTmoey4XGfuiOIt8FCi' -O data/obligation_extraction_df.csv
 
 mkdir models
 wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=19mU-5kFpMcMiOZfvD98jN8v814HEdz7X' -O models/xgb_tfidf.joblib
