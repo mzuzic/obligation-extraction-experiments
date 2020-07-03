8a04b84ca2bf084d7aa31e59c1eca9041594f9a4
markozuzic <mzuzic@apttus.com>
2020-07-03 15:23:57 +0200

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
-mkdir data_test
-wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1sq43KgDLrZZ3mdTmoey4XGfuiOIt8FCi' -O data/obligation_extraction_df.csv
+mkdir data
+wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=1sq43KgDLrZZ3mdTmoey4XGfuiOIt8FCi' -O data/obligation_extraction_df.csvi
 
 mkdir models
 wget --no-check-certificate -r 'https://docs.google.com/uc?export=download&id=19mU-5kFpMcMiOZfvD98jN8v814HEdz7X' -O models/xgb_tfidf.joblib
